ba57fe458007d527e8b43a1efe8423db65730668
cmibarnwell <calebbarnwell@gmail.com>
2017-04-07 14:17:19 -0500

    Time to try again

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :admins
-  mount Ckeditor::Engine => '/ckeditor'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
-
-  resources :posts, :pages
-end
--- db/migrate/20170329140339_add_avatar_to_admins.rb
+++
@@ -1,5 +0,0 @@
-class AddAvatarToAdmins < ActiveRecord::Migration[5.0]
-  def change
-    add_column :admins, :avatar, :string
-  end
-end
