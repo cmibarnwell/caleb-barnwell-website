8bc4d002b5eb50c51980f7f86b0a83917b7efa96
cmibarnwell <calebbarnwell@gmail.com>
2017-07-05 16:51:08 -0500

    hamburger icon slides, implemented data-attributes

--- app/assets/javascripts/left_nav.js
+++ app/assets/javascripts/left_nav.js
@@ -3,22 +3,14 @@
  */
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    var mq = window.matchMedia( "(min-width: 500px)" );
-    if(mq.matches){
-        document.getElementById("leftNav").style.width = "25%";
-        document.getElementsByTagName('MAIN')[0].style.marginLeft = "25%";
-    }else{
-        document.getElementById("leftNav").style.width = "40%";
-        document.getElementsByTagName('MAIN')[0].style.marginLeft = "40%";
-    }
-    document.getElementById("open").style.display = 'none';
-    document.getElementById("close").style.display = 'inline-block';
+    document.getElementsByTagName("NAV")[0].dataset.view = "open";
+    document.getElementsByTagName("MAIN")[0].dataset.view = "reduced";
+    document.getElementById("navToggle").setAttribute("onclick", "closeNav()");
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("leftNav").style.width = "0";
-    document.getElementsByTagName('MAIN')[0].style.marginLeft = "0";
-    document.getElementById("open").style.display = 'inline-block';
-    document.getElementById("close").style.display = 'none';
+    document.getElementsByTagName("NAV")[0].dataset.view = "closed";
+    document.getElementsByTagName("MAIN")[0].dataset.view = "widened";
+    document.getElementById("navToggle").setAttribute("onclick", "openNav()");
 }
