f425bef80824be459dedbc0e11dcc3bccdacdf87
cmibarnwell <calebbarnwell@gmail.com>
2017-07-05 13:03:40 -0500

    replaced div id=main with html5 main tag

--- app/assets/javascripts/left_nav.js
+++ app/assets/javascripts/left_nav.js
@@ -6,10 +6,10 @@ function openNav() {
     var mq = window.matchMedia( "(min-width: 500px)" );
     if(mq.matches){
         document.getElementById("leftNav").style.width = "25%";
-        document.getElementById("main").style.marginLeft = "25%";
+        document.getElementsByTagName('MAIN')[0].style.marginLeft = "25%";
     }else{
         document.getElementById("leftNav").style.width = "40%";
-        document.getElementById("main").style.marginLeft = "40%";
+        document.getElementsByTagName('MAIN')[0].style.marginLeft = "40%";
     }
     document.getElementById("open").style.display = 'none';
     document.getElementById("close").style.display = 'inline-block';
@@ -18,7 +18,7 @@ function openNav() {
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("leftNav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementsByTagName('MAIN')[0].style.marginLeft = "0";
     document.getElementById("open").style.display = 'inline-block';
     document.getElementById("close").style.display = 'none';
 }
