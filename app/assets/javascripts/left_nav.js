e0b148acb2234cc64311d736569df4532a33b48f
cmibarnwell <calebbarnwell@gmail.com>
2017-07-06 14:43:07 -0500

    improved markup

--- app/assets/javascripts/left_nav.js
+++ app/assets/javascripts/left_nav.js
@@ -4,13 +4,13 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementsByTagName("NAV")[0].dataset.view = "open";
-    document.getElementsByTagName("MAIN")[0].dataset.view = "reduced";
+    document.getElementsByTagName("BODY")[0].dataset.view = "reduced";
     document.getElementById("navToggle").setAttribute("onclick", "closeNav()");
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementsByTagName("NAV")[0].dataset.view = "closed";
-    document.getElementsByTagName("MAIN")[0].dataset.view = "widened";
+    document.getElementsByTagName("BODY")[0].dataset.view = "widened";
     document.getElementById("navToggle").setAttribute("onclick", "openNav()");
 }
