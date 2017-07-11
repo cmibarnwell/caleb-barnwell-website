4860d337128597cca38f6fa019dc2c6bb144ef2d
cmibarnwell <calebbarnwell@gmail.com>
2017-07-11 10:40:34 -0500

    sticky footer implementation, css fixes in progress

--- app/assets/javascripts/left_nav.js
+++ app/assets/javascripts/left_nav.js
@@ -4,13 +4,15 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementsByTagName("NAV")[0].dataset.view = "open";
-    document.getElementsByTagName("BODY")[0].dataset.view = "reduced";
+    document.getElementsByTagName("MAIN")[0].dataset.view = "reduced";
+    document.getElementsByTagName("FOOTER")[0].dataset.view = "reduced";
     document.getElementById("navToggle").setAttribute("onclick", "closeNav()");
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementsByTagName("NAV")[0].dataset.view = "closed";
-    document.getElementsByTagName("BODY")[0].dataset.view = "widened";
+    document.getElementsByTagName("MAIN")[0].dataset.view = "widened";
+    document.getElementsByTagName("FOOTER")[0].dataset.view = "widened";
     document.getElementById("navToggle").setAttribute("onclick", "openNav()");
 }
