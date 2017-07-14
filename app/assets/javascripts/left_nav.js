e39c52723647e62b19fb02660733597abcf71c98
cmibarnwell <calebbarnwell@gmail.com>
2017-07-14 15:37:06 -0500

    header implemented

--- app/assets/javascripts/left_nav.js
+++ app/assets/javascripts/left_nav.js
@@ -3,16 +3,26 @@
  */
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementsByTagName("NAV")[0].dataset.view = "open";
-    document.getElementsByTagName("MAIN")[0].dataset.view = "reduced";
+    document.getElementById("leftNav").className = "open";
+    document.getElementsByTagName("MAIN")[0].className = "reduced";
+    document.getElementsByTagName("MAIN")[0].setAttribute("onclick", "closeNav()");
     document.getElementsByTagName("FOOTER")[0].dataset.view = "reduced";
     document.getElementById("navToggle").setAttribute("onclick", "closeNav()");
+    document.getElementsByTagName("NAV")[0].classList.remove("closed");
+    document.getElementsByTagName("NAV")[0].classList.add("open");
+    document.getElementById("navToggle").classList.remove("fa-bars");
+    document.getElementById("navToggle").classList.add("fa-close");
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementsByTagName("NAV")[0].dataset.view = "closed";
-    document.getElementsByTagName("MAIN")[0].dataset.view = "widened";
+    document.getElementById("leftNav").className = "closed";
+    document.getElementsByTagName("MAIN")[0].className = "widened";
+    document.getElementsByTagName("MAIN")[0].setAttribute("onclick", "");
     document.getElementsByTagName("FOOTER")[0].dataset.view = "widened";
     document.getElementById("navToggle").setAttribute("onclick", "openNav()");
+    document.getElementsByTagName("NAV")[0].classList.remove("open");
+    document.getElementsByTagName("NAV")[0].classList.add("closed");
+    document.getElementById("navToggle").classList.remove("fa-close");
+    document.getElementById("navToggle").classList.add("fa-bars");
 }
