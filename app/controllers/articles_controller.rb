a0c9a9d07da14435051331db22ee4e5a7456104c
cmibarnwell <calebbarnwell@gmail.com>
2017-10-20 11:00:25 -0500

    Added authors to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.author = current_user.username
     @article.save
 
     flash.notice = "Article #{@article.title} Created!"
